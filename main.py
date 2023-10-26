275da8d5ef32083e82e950ab3b201da6f8d1bde9
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-10-26 08:41:48 +0530

    1 issue fixed

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import random
 
 moves = ["R", "L", "U", "D", "B", "F", "R'", "L'",
          "U'", "D'", "B'", "F'", "R2", "L2", "U2", "D2", "B2", "F2"]
+prev_move = ""
+n = int(input("Length of your scramble: "))
 
-for i in range(1, 20):
+for i in range(1, n):
     move = random.choice(moves)
+    if move == prev_move:
+        n += 1
+        continue
+    prev_move = move
     print(move, end=" ")
