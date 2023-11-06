4f7a0dd77ec2c36dd04fbd0ca1c694d9263e66f1
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-11-06 22:54:16 +0530

    completed

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import random
 moves = ["R", "L", "U", "D", "B", "F", "R'", "L'",
          "U'", "D'", "B'", "F'", "R2", "L2", "U2", "D2", "B2", "F2"]
 prev_move = ""
+
 n = int(input("Length of your scramble: "))
 
-for i in range(1, n):
+for i in range(0, n):
     move = random.choice(moves)
     if move == prev_move:
         n += 1
         continue
-    elif move == prev_move + "'":
-        n += 1
-        continue
+
+    if i != 0:
+        if move == prev_move[0]:
+            n += 1
+            continue
+
     prev_move = move
     print(move, end=" ")
